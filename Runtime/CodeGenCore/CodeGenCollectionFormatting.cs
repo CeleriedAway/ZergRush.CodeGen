15dee8f41bc02439c2c8694d68c49f2a8a3ee740
Alexandr Pyatkov <myinkmonster@gmail.com>
2026-07-20 13:35:29 +0300

    fix(unity): support C# 9 compiler

--- Runtime/CodeGenCore/CodeGenCollectionFormatting.cs
+++ Runtime/CodeGenCore/CodeGenCollectionFormatting.cs
@@ -2,54 +2,55 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ZergRush.CodeGen;
-
-public static class CodeGenCollectionFormatting
+namespace ZergRush.CodeGen
 {
-    public static string ToCodeGenListString<T>(this IEnumerable<T> collection, string delimiter = ", ")
+    public static class CodeGenCollectionFormatting
     {
-        if (collection == null) return "empty collection";
-        return string.Join(delimiter, collection.Select(val => val?.ToString()).ToArray());
-    }
-
-    public static int IndexOf<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
-    {
-        var index = 0;
-        foreach (var item in collection)
+        public static string ToCodeGenListString<T>(this IEnumerable<T> collection, string delimiter = ", ")
         {
-            if (predicate(item)) return index;
-            index++;
+            if (collection == null) return "empty collection";
+            return string.Join(delimiter, collection.Select(val => val?.ToString()).ToArray());
         }
 
-        return -1;
-    }
-
-    public static T TakeAt<T>(this IList<T> list, int index)
-    {
-        var value = list[index];
-        list.RemoveAt(index);
-        return value;
-    }
-
-    public static int InsertSorted<T>(this IList<T> list, T value, Func<T, int> order)
-    {
-        var valueOrder = order(value);
-        var index = list.IndexOf(item => order(item) >= valueOrder);
-        if (index < 0)
+        public static int IndexOf<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
         {
-            list.Add(value);
-            return list.Count - 1;
+            var index = 0;
+            foreach (var item in collection)
+            {
+                if (predicate(item)) return index;
+                index++;
+            }
+
+            return -1;
         }
 
-        list.Insert(index, value);
-        return index;
-    }
-
-    public static void EnsureSize<T>(this List<T> list, int count) where T : new()
-    {
-        while (list.Count < count)
+        public static T TakeAt<T>(this IList<T> list, int index)
         {
-            list.Add(new T());
+            var value = list[index];
+            list.RemoveAt(index);
+            return value;
+        }
+
+        public static int InsertSorted<T>(this IList<T> list, T value, Func<T, int> order)
+        {
+            var valueOrder = order(value);
+            var index = list.IndexOf(item => order(item) >= valueOrder);
+            if (index < 0)
+            {
+                list.Add(value);
+                return list.Count - 1;
+            }
+
+            list.Insert(index, value);
+            return index;
+        }
+
+        public static void EnsureSize<T>(this List<T> list, int count) where T : new()
+        {
+            while (list.Count < count)
+            {
+                list.Add(new T());
+            }
         }
     }
 }
